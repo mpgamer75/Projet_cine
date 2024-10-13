f787146591bee1fe9b9d922beada0e2e458f1199
Charles <charleslantiguajorge@gmail.com>
2024-10-13 17:11:29 +0200

    ajout des fichiers et API REST sur le dossiers index.js et users.js

--- index.js
+++ index.js
@@ -2,10 +2,38 @@ const express = require('express');
 const app=express();
 const port = 3000;
 
-app.get('/',(req,res)=>{
-    res.send("Hello World this is my first API");
+let users = [
+    {id:1, name : 'Charles'},
+    {id:2, name : 'Karina'},
+    {id: 3, name: 'Chaewon'},
+];
+
+app.use(express.json());
+
+app.get('/users',(req,res)=>{ // récupère tous les users 
+    res.json(users);
 });
 
-app.listen(port,()=>{
-    console.log('Server is running at http://localhost:${port}'); // le $ est du jQuery ==> prend tous les arguments 
+app.get('users/id:',(req,res)=>{
+    const user=users.find(u=>u.id ===parseInt(req.params.id));
+    if(!users) return res.status(404).send('User not found');
+    res.json(user);
 });
+
+app.post('/users',(req,res)=>{
+    const user = users.find(u=>u.id ===parseInt(req.params.id));
+    if(!users) return res.status(404).send('User not found');
+    user.name = req.body.name;
+    res.json(user);
+});
+
+app.delete('/users/:id', (req, res) => {
+    const userIndex = users.findIndex(u => u.id === parseInt(req.params.id));
+    if (userIndex === -1) return res.status(404).send('User not found');
+    users.splice(userIndex, 1);
+    res.status(204).send();
+  });
+  
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
